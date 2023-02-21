165cc2c451380ceca08aa2660dc5166b2650dec0
eMobilis <b649070@gmail.com>
2023-02-21 15:23:59 +0300

    first commit

---
+++ data_types.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter any number"))
+num2 = int(input("enter a second number"))
+print(num1)
+
---
+++ variables.py
@@ -0,0 +1,7 @@
+age = "76"
+home = "kawangware"
+hobby = "fifa"
+name = "jonh"
+talent = "nothing"
+
+print(f"my name is {name} and im {age} year,,,from {home}")
